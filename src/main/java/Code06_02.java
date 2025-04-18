545624d016b6678e58da54c224f12b5342855dff
taehun02 <taehun020505@gmail.com>
2025-04-18 12:30:03 +0900

    변경

---
+++ src/main/java/Code06_02.java
@@ -0,0 +1,7 @@
+public class Code06_02 {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++) {
+            System.out.println("난생처음 자바는 재미있습니다. ^^");
+        }
+    }
+}
