ea72b5cbdc462e16c7041fc34e9ccb56a7cab96a
taehun02 <taehun020505@gmail.com>
2025-04-25 15:47:28 +0900

    변경

---
+++ src/main/java/Code07_02.java
@@ -0,0 +1,11 @@
+public class Code07_02 {
+    public static void main(String[] args) {
+        String mystr = "My First Life";
+        String var1;
+
+        var1 = mystr.toUpperCase();
+        System.out.println(var1);
+        System.out.println(var1.length());
+        System.out.println(var1.isEmpty());
+    }
+}
