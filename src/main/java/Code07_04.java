27404a22327abeba600c22baf63df30affad53ce
taehun02 <taehun020505@gmail.com>
2025-04-27 21:59:15 +0900

    변경

---
+++ src/main/java/Code07_04.java
@@ -0,0 +1,16 @@
+class Rabbit {
+    String shape;
+    int xPos;
+    int yPos;
+
+    void setPosition(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+}
+
+public class Code07_04 {
+    public static void main(String[] args) {
+
+    }
+}
