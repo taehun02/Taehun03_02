7fc2d5afbdd0b1f324bedacdad9330fdd0ff51c2
taehun02 <taehun020505@gmail.com>
2025-04-27 22:03:05 +0900

    변경

---
+++ src/main/java/Code07_07.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Code07_07 {
+    // Code07_06.java의 2~9행과 동일
+}
+
+public class Code07_07 {
+    public static void main(String[] args) {
+        Rabbit rabbit1 = new Rabbit();
+        Rabbit rabbit2 = new Rabbit();
+        Rabbit rabbit3 = new Rabbit();
+
+    }
+}
